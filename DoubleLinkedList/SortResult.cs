0455c3ebe8078cb3e25f0c4e12dd4e09386ac46d
smovzhai <36729565+smovzhai@users.noreply.github.com>
2019-04-19 15:12:20 +0300

    Remove not used constructor

--- DoubleLinkedList/SortResult.cs
+++ DoubleLinkedList/SortResult.cs
@@ -5,17 +5,7 @@ using System.Linq;
 namespace DoubleLinkedList
 {
     public class SortResult
-    {
-        public SortResult()
-        {
-        }
-
-        public SortResult(int comparisonsCount, int swapsCount)
-        {
-            ComparisonsCount = comparisonsCount;
-            SwapsCount = swapsCount;
-        }
-
+    {   
         public int ComparisonsCount { get; private set; }
 
         public int SwapsCount { get; private set; }
